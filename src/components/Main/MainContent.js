039261e9defe0e5a21998f322cd63ed84324ce21
Yaiza <yaizavallejo16@gmail.com>
2021-08-07 20:08:44 +0200

    Add main-site-header and add styles

---
+++ src/components/Main/MainContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './MainContent.scss'
+
+function MainContent() {
+    return (
+        <div className="main-site">
+            <div className="main-site-header">
+                <h1>Stays in Finland</h1>
+                <p>12+ stays</p>
+            </div>
+        </div>
+    )
+}
+
+export default MainContent
