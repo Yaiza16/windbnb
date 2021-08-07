74f36b85ed630b4164225eaa32017cda66724ad4
Yaiza <yaizavallejo16@gmail.com>
2021-08-07 20:02:24 +0200

    Add MainContent component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header';
 import './App.scss';
+import MainContent from './components/Header/MainContent';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <MainContent />
     </div>
   );
 }
