2392d6710ec48b66663d87b7b3b65825355e06a5
Yaiza <yaizavallejo16@gmail.com>
2021-08-08 11:09:02 +0200

    Update: add href

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Logo() {
     return (
-        <div className="logo-container">
+        <a href="#!" className="logo-container">
             <img src="assets/images/logo.svg" alt="site-logo" />
-        </div>
+        </a>
     )
 }
 
